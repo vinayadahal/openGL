d2711107c96aa6b59724cf5101bccba99661566e
Vinaya Dahal <vinayadahal@gmail.com>
2016-10-02 18:53:23 +0545

    Practicing OpenGL

---
+++ app/src/main/java/com/mediaplayer/opengl/MainActivity.java
@@ -0,0 +1,36 @@
+package com.mediaplayer.opengl;
+
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GLSurfaceView glSurfaceView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        glSurfaceView = new GLSurfaceView(this);
+        glSurfaceView.setRenderer(new VideoRenderer());
+        setContentView(glSurfaceView);
+//        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        glSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        glSurfaceView.onPause();
+    }
+
+}
